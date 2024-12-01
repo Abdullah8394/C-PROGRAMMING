aa7d692263560c212d6182b909b7808a0ffe336f
Abdullah8394 <khanabdulla@eng.rizvi.edu.in>
2024-12-02 01:03:13 +0530

    Update TASK-2.c

---
+++ EXP-10/TASK-2.c
@@ -0,0 +1,50 @@
+/*
+    EXP No.10 : TASK-1
+    Title: WAP to print the elements of an array in reverse order using pointers
+    Name: KHAN ABDULLA ABDUL REHMAN
+    Class & Div: FE & E
+    UIN: 241S055
+
+*/
+
+
+#include <stdio.h>
+
+  int main() {
+    int n;
+
+
+    printf("Enter the number of elements in the array: ");
+    scanf("%d", &n);
+
+
+    int arr[n];
+    int *ptr = arr;
+
+
+    printf("Enter %d elements:\n", n);
+    for (int i = 0; i < n; i++) {
+     scanf("%d", ptr + i);
+    }
+
+
+    printf("Array elements in reverse order:\n");
+    for (int i = n - 1; i >= 0; i--) {
+     printf("%d ", *(ptr + i));
+    }
+
+    printf("\n");
+
+
+    return 0;
+}
+
+/*  **OUTPUT**
+
+Enter the number of elements in the array: 4
+
+Enter 4 elements: 3 5 7 9
+
+Array elements in reverse order: 9 7 5 3
+
+*/
